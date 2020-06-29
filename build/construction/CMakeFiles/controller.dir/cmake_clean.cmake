a95ffc64cd036795bdbd5f87b4c7a96800858a6e
cbrosque <46803259+cbrosque@users.noreply.github.com>
2020-06-29 16:06:15 -0700

    Add files via upload

---
+++ build/construction/CMakeFiles/controller.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/controller.dir/controller.cpp.o"
+  "../../bin/construction/controller.pdb"
+  "../../bin/construction/controller"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/construction/CMakeFiles/simulation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simulation.dir/simulation.cpp.o"
+  "../../bin/construction/simulation.pdb"
+  "../../bin/construction/simulation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
