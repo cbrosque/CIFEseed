d878307218c32974cf6fb7d97823a5ff078b1838
cbrosque <cbrosque@stanford.edu>
2020-09-29 20:46:49 -0700

    new simviz works

--- joint/simviz2.cpp
+++ joint/simviz2.cpp
@@ -20,7 +20,7 @@ void sighandler(int){fSimulationRunning = false;}
 using namespace std;
 using namespace Eigen;
 
-const string world_file = "./resources/world.urdf";
+const string world_file = "./resources/world2.urdf";
 const string robot_file = "./resources/mmp_panda.urdf";
 const string obj_file = "mmp_panda_tool_no_collision.urdf";
 const string robot_name = "mmp_panda";
